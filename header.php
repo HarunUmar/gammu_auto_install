75ab7bdfa9cabc942523bbc0feb8dffed8867605
HarunUmar <nelayan.ikan7@gmail.com>
2015-01-30 10:05:50 +0800

    Gammu Auto Installer

---
+++ header.php
@@ -0,0 +1,14 @@
+<h1>Easy Gammu Installer V2.0</h1>
+
+
+<hr>
+<p>
+<ul>
+<li><a href="step1.php"><b>Langkah 1</b></a>: Setting Database</li>
+<li><a href="step2.php"><b>Langkah 2</b></a>: Setting Phone/Modem<br><br></li>
+<li><b><a href="kirim.php">Test Kirim SMS</a></b></li>
+<li><b><a href="terima.php">Test Terima SMS</a></b></li>
+</ul>
+</p>
+<hr>
+
